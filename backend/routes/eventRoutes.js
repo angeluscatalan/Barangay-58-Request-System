8355c6bcadae1899954cada6b2deff2259f4f062
David Pomasin <davidpomasinsp@gmail.com>
2025-03-31 17:05:13 +0800

    final na today pag ayaw parin pahinga muna ako hehe

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -9,7 +9,7 @@ router.post('/archive', upload.single('image'), eventsController.archiveEvent);
 router.get('/archive', eventsController.getArchivedEvents);
 router.delete('/archive/:id', eventsController.deleteArchivedEvent);
 router.post('/upload', upload.single('image'), eventsController.uploadEvent);
-router.get("/events", eventsController.getEvents);
+router.get("/", eventsController.getEvents);
 
 
 module.exports = router;
